bf52c4e38b0e78853ebb2351ee41e3ac7f4c4563
Computer 3 <lindberghrobotics@gmail.com>
2020-02-17 10:59:32 -0800

    Tai-Added commands and changed yaml file

--- src/main/java/frc/robot/commands/ManualClimbCommand.java
+++ src/main/java/frc/robot/commands/ManualClimbCommand.java
@@ -43,6 +43,7 @@ public class ManualClimbCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.climb.setClimbLiftMotor(Robot.oi.operatorController.getRawAxis(Robot.oi.LEFT_THUMB));
     }
 
     // Make this return true when this Command no longer needs to run execute()
